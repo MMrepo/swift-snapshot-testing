c62b745c3449de0e05080193e5099aaae690e1b2
Mateusz Malek <it.mateuszmalek@gmail.com>
2019-08-30 09:38:47 +0200

    Add iOS 13 availability check

--- Sources/SnapshotTesting/Common/View.swift
+++ Sources/SnapshotTesting/Common/View.swift
@@ -663,17 +663,24 @@ func prepareView(
   let traits = UITraitCollection(traitsFrom: [config.traits, traits])
   let window: UIWindow
   if drawHierarchyInKeyWindow {
-    guard let keyWindow = UIApplication.shared.connectedScenes
+    if #available(iOS 13.0, *) {
+      guard let keyWindow = UIApplication.shared.connectedScenes
         .filter({$0.activationState == .foregroundActive})
         .map({$0 as? UIWindowScene})
         .compactMap({$0})
         .first?.windows
         .filter({$0.isKeyWindow}).first else {
-      fatalError("'drawHierarchyInKeyWindow' requires tests to be run in a host application")
+          fatalError("'drawHierarchyInKeyWindow' requires tests to be run in a host application")
+       } 
+      window = keyWindow
+      window.frame.size = size
+     } else {
+      guard let keyWindow = UIApplication.shared.keyWindow else {
+         fatalError("'drawHierarchyInKeyWindow' requires tests to be run in a host application")
+      }
+      window = keyWindow
+      window.frame.size = size
     }
-    
-    window = keyWindow
-    window.frame.size = size
   } else {
     window = Window(
       config: .init(safeArea: config.safeArea, size: config.size ?? size, traits: traits),
